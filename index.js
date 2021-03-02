372abf0715ab5880fcb22bcc5c2293905f7fa646
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-02 01:41:07 +0100

    Added first Mongoose logic to "create" user.

--- index.js
+++ index.js
@@ -92,20 +92,40 @@ app.get('/', (req, res) => {
     res.send('Director biography goes here');
   });
 
-  //Adds data for a new user to our list of users.
-  //A JSON object handling data about the user to be added.
+  //Add a user
+  /* We’ll expect JSON in this format
+  {
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+  }*/
   app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.username) {
-    const message = 'Missing name in request body';
-    res.status (400).send(message);
-  } else {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
-});
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 //FIX!!!!.
   app.put('/users/:username', (req, res) => {
