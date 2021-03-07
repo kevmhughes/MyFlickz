b95f2347f3f12b53e8f309572eb1b4647eca98ab
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-07 15:32:40 +0100

    environmental variable modifieed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User;
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
 
 //This is the online database - using environmental variable "CONNECTION_URI" to avoid exposing the connection URI
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 
 bodyParser = require('body-parser'),
