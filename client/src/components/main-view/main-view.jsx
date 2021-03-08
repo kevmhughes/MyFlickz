f70ea28cc982e815cb3c829818e99bd78ac45aa3
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-08 17:55:01 +0100

    componentDidMount method added to main-view.jsx, export class added to main-view.jsx, and MainView imported to index.jsx.

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
     constructor() {
 
         // Call the superclass constructor 
@@ -13,11 +13,35 @@ class MainView extends React.Component {
         this.state = {};
     }
 
+    componentDidMount() {
+        axios.get('https://myflickz.herokuapp.com/movies')
+        .then(response => {
+            // Assign the result to the state
+            this.setState({
+                movies:response.data
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     // This overrides the render() method of the superclass
     // No need to call super() though, as it does nothin by default
     render() {
+        // If the state is not initialized, this will throw on runtime
+        // before the data is initially loaded
+        const { movies } = this.state;
+
+        //Before the movies have been laoded
+        if (!movies) return <div className="main-view"/>;
+
         return (
-            <div className="main-view"></div>
+            <div className="main-view">
+                {movies.map(movie => (
+                    <div className="movie-card" key={movie._id}>{movie.Title}</div>
+                ))}
+            </div>
         );
     }
 }
