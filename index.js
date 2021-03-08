ebac449b2bbd7ce7dbca8fcd26c8985d8c30475f
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-08 17:34:33 +0100

    Authentication logic removed from /movies endpoint.

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/users/:Username', (req, res) => {
 });
 
 // Get a list of all the movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
