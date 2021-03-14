a7c7b1fe211b2e5aa41deb7290b10eea41805bf7
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-14 04:16:41 +0100

    modified index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/users/:Username', (req, res) => {
 });
 
 // Get a list of all the movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
