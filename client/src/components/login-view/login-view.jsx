9a3bf1bbf0c85e910ea043fdc4581a5b55455fc1
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-03-29 18:41:21 +0200

    Routing login-view

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -18,6 +18,7 @@ export function LoginView(props) {
     .then(response => {
       const data = response.data;
       props.onLoggedIn(data);
+      window.open('/client', '_self');// the second argument '_self' is necessary so that the page will open in the current tab
     })
     .catch(e => {
       alert('that user does not exist');
@@ -74,7 +75,7 @@ export function LoginView(props) {
             <br/>
             <br/>
             <span style={{width: "100%"}}>
-              Not registered? <a href='/users' style={{textDecoration: "none"}}>Create an account</a>
+              Not registered? <a href='client/users' style={{textDecoration: "none"}}>Create an account</a>
             </span>
           
           </Form>
